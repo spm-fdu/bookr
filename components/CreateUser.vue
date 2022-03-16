1881f3f8d788da7b30ed16dd543b246c75c7274e
nngg1104 <nngg1104@gmail.com>
2022-03-17 00:05:01 +0100

    Register mail with password by clicking auth button

---
+++ components/CreateUser.vue
@@ -0,0 +1,18 @@
+<template></template>
+<script>
+export default {
+  name: "CreateUser",
+  methods: {
+    async createUser() {
+      try {
+        await this.$fire.auth.createUserWithEmailAndPassword(
+          "foo@foo.foo",
+          "test"
+        );
+      } catch (e) {
+        handleError(e);
+      }
+    },
+  },
+};
+</script>
--- pages/authentication.vue
+++ pages/authentication.vue
@@ -1,57 +1,63 @@
 <template>
   <v-container fluid fill-height>
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col
-        class="text-center"
-        cols="12"
-      >
-        <h1 class="page__title">
-          <span>lol</span>.
-        </h1>
-        <h4 class="subheading font-weight-light"> 
-          Testing!
-        </h4>
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="page__title"><span>Auth</span>.</h1>
+        <h4 class="subheading font-weight-light">Testing authentication.</h4>
       </v-col>
       <v-col class="text-center mt-6">
         <nuxt-link :key="$router.fullPath" to="./">
           <v-btn depressed outlined rounded class="pl-9 pr-9" color="#3b47ec">
-            Test
+            Home
           </v-btn>
         </nuxt-link>
+        <v-btn @click="createUser()" depressed outlined rounded class="pl-9 pr-9" color="#3b47ec">
+            Register
+          </v-btn>
+        <div id="example"></div>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import CreateUser from "../components/CreateUser.vue";
 export default {
-  name: 'Booking'
-}
+  name: "Authentication",
+  methods: {
+    async createUser() {
+      try {
+        await this.$fire.auth.createUserWithEmailAndPassword(
+          "spm.fudan@gmail.com",
+          "password"
+        );
+      } catch (e) {
+        handleError(e);
+      }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
+.page__title {
+  font-family: "Playfair-Display", serif;
+  font-weight: 600;
+}
+
+@media (min-width: $large-screen-size) {
   .page__title {
-    font-family: 'Playfair-Display', serif;
-    font-weight: 600;
+    font-size: 6em;
   }
-
-  @media (min-width: $large-screen-size) {
-    .page__title {
-      font-size: 6em
-    }
+}
+@media (min-width: $medium-screen-size) {
+  .page__title {
+    font-size: 8em;
   }
-  @media (min-width: $medium-screen-size) {
-    .page__title {
-      font-size: 8em
-    }
+}
+@media (max-width: $small-screen-size) {
+  .page__title {
+    font-size: 10em;
   }
-  @media (max-width: $small-screen-size) {
-    .page__title {
-      font-size: 10em;
-    }
-  }
-
+}
 </style>
