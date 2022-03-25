6ff99db407b186e048bdddfba71a3b255fd7f162
sztoo <stoo@hawk.iit.edu>
2022-03-25 20:08:54 +0800

    Add page header

---
+++ components/PageHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="header__login text-overline text-right pt-10 pr-16 text-decoration-none">
+        <span>
+            <nuxt-link to="/signup" class="text-overline text-decoration-none">sign up</nuxt-link>
+        </span>
+        <span>/</span>
+        <span>
+            <nuxt-link to="/login" class="text-overline text-decoration-none">login</nuxt-link>
+        </span>
+    </div>
+  </div>
+</template>
+
+<style>
+.header__login a { 
+  transition: 0.3s;
+  color: grey;
+}
+.header__login a:hover { 
+  color: #3b47ec
+}
+</style>
