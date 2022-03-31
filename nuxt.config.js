8dfb257c33aac032c74de4fc8444ad3f31e54c85
sztoo <stoo@hawk.iit.edu>
2022-03-31 13:11:04 +0800

    BOOK-33 Add basic chart for dashboard

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    {src:'~/plugins/persist.client.js'}
+    { src:'~/plugins/persist.client.js', ssr:false },
+    { src:"~/plugins/carbon.client.js", ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/carbon.client.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import "@carbon/charts/styles.css";
+import chartsVue from "@carbon/charts-vue";
+
+import "~/assets/plex-and-carbon-components.css";
+Vue.use(chartsVue);
