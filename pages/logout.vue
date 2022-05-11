c77e8055b4ca5d2d4c1b90a39e32c43f3d35ce43
sztoo <stoo@hawk.iit.edu>
2022-05-11 10:25:28 +0800

    BOOK-31 Logout

---
+++ pages/logout.vue
@@ -0,0 +1,22 @@
+<template></template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    ...mapActions(['clearPersist'])
+  },
+  
+  mounted () {
+    console.log('logging out...')
+    this.$fire.auth.signOut(this.$fire.auth).then(() => {
+      this.clearPersist();
+      this.$router.push('/');
+    }).catch((e) => {
+      console.log('Sign out failed.');
+      console.log(`Error message: ${e}`);
+    })
+  }
+}
+</script>
