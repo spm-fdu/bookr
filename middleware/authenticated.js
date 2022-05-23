0b69ca168a27258a84558f4dcf0614d7bf03f754
sztoo <stoo@hawk.iit.edu>
2022-05-23 18:12:56 +0800

    BOOK-34 add reroute logic

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,6 +1,27 @@
-export default function ({ store, redirect }) {
-  // If the user is not authenticated
-  if (!store.state.persisted.authenticated) {
-    return redirect('/login')
+export default function ({ store, redirect, route }) {
+  let payload = localStorage.getItem('vuex');
+  if (payload) {
+    payload = JSON.parse(payload)['persisted'];
+  } else {
+    // first time logging in 
+    // return redirect('/login')
+    return Promise.resolve();
   }
+
+  // signed in before but signed out 
+  if (payload.authenticated === false && store.state.persisted.authenticated === false) {
+    return Promise.resolve();
+  } else {
+    // user is authenticated 
+    // reinject into store 
+    store._actions['persist'][0](payload);
+
+    // source 
+    if (route.fullPath == '/login' || route.fullPath == '/signup') {
+      return redirect('/dashboard')
+    }
+
+    return Promise.resolve();
+  }
+
 }
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid fill-height class="bookr__login ml-8 mr-8">
+  <v-container fluid class="bookr__login ml-8 mr-8 mt-16 mb-16">
     <v-row class="text-center justify-center align-center">
       <v-col cols="7">
         <img src="../static/login.svg" :style="`pointer-events:none`"/>
@@ -13,22 +13,49 @@
 
 <script>
 import BookrLogin from '../components/BookrLogin.vue'
+// import { mapState, mapActions } from 'vuex';
 
 export default {
   components: {
     BookrLogin,
   },
-  mounted () {
-    // route guard
-    this.verifyLoginStatus();
-  },
-  methods: {
-    verifyLoginStatus () {
-      if (this.$fire.auth.currentUser) {
-        this.$router.push('/dashboard');
-      }
-    }
-  }
+  middleware: 'authenticated'
+  // mounted () {
+  //   // route guard
+  //   this.verifyLoginStatus();
+  // },
+  // computed: {
+  //   ...mapState(['persisted'])
+  // },
+  // methods: {
+  //   ...mapActions(['persist'])
+  // },
+
+  // mounted () {
+  //   if ((this.persisted.authenticated === true) && (this.persisted.email.length > 0)) {
+  //     const payload = {
+  //       fname: this.persisted.fname,
+  //       lname: this.persisted.lname,
+  //       email: this.persisted.email,
+  //       authenticated: this.persisted.authenticated
+  //     }
+  //     this.persist(payload)
+  //     this.$router.push('/dashboard')
+  //     // this.$store.state.persisted.fname = this.persisted.fname;
+  //     // this.$store.state.persisted.lname = this.persisted.lname; 
+  //     // this.$store.state.persisted.email = this.persisted.email;
+  //     // this.$store.state.persisted.authenticated = this.persisted.authenticated;
+  //   } else {
+  //     console.log(this.persisted)
+  //   }
+  // },
+  // methods: {
+  //   verifyLoginStatus () {
+  //     if (this.$fire.auth.currentUser) {
+  //       this.$router.push('/dashboard');
+  //     }
+  //   }
+  // }
 }
 </script>
 
